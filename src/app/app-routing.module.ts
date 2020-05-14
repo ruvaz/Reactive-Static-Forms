8030458d8bab4f43e6c6efcc3e9482aa9c670c39
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2020-05-14 10:42:22 -0500

    FormsModule added, form template and reactive, routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ReactiveComponent} from './pages/reactive/reactive.component';
+import {TemplateComponent} from './pages/template/template.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reactive', component: ReactiveComponent},
+  {path: 'template', component: TemplateComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'template'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
